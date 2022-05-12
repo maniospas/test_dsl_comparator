1398e196f70d17c868e4f3c9eb06e7c774d0c2da
Emmanouil (Manios) Krasanakis <maniospas@hotmail.com>
2022-05-12 14:57:17 -0700

    Update method_components_nofop.py

--- method_components_nofop.py
+++ method_components_nofop.py
@@ -1,19 +1,27 @@
+from pyfop import lazy, autoaspects, Aspect, Priority
 import numpy as np
 
 
+@lazy
 def tautology(x):
     return x
 
 
-def normalize(x, norm=2):
+@lazy
+def normalize(x, norm=Aspect(2, Priority.LOW)):
     return x / (np.sum(x**norm))**(1./norm)
 
 
+@lazy
 def dot(x, y):
     return np.sum(x*y)
 
 
-def KLdivergence(x, y, epsilon=np.finfo(float).eps):
+@lazy
+@autoaspects
+def KLdivergence(x, y, norm=1, epsilon=np.finfo(float).eps):
+    if norm != 1:
+        raise Exception("KLDivergence should not work on non-L1 normalizations")
     return np.sum(x*np.log(x/(y+epsilon)+epsilon))
 
 
@@ -24,24 +32,9 @@ class Similarity:
         self.kwargs = kwargs
 
     def __call__(self, x, y):
-        if self.transform == normalize and self.measure == KLdivergence:
-            norm = self.kwargs.get("norm", 1)
-            if norm != 1:
-                raise Exception("KLDivergence should not work on non-L1 normalizations")
-            x = self.transform(x, norm=norm)
-            y = self.transform(y, norm=norm)
-        elif self.transform == normalize:
-            norm = self.kwargs.get("norm", 2)
-            x = self.transform(x, norm=norm)
-            y = self.transform(y, norm=norm)
-        else:
-            x = self.transform(x)
-            y = self.transform(y)
-        if self.measure == KLdivergence and "epsilon" in self.kwargs:
-            return self.measure(x, y, epsilon=self.epsilon)
-        return self.measure(x, y)
+        return self.measure(self.transform(x), self.transform(y)).call(**self.kwargs)
 
 
 x = np.array([1., 1., 1.])
 y = np.array([1., 0., 1.])
-print(Similarity(normalize, KLdivergence, norm=1)(x, y))
+print(Similarity(normalize, KLdivergence, norm=1, epsilon=Aspect())(x, y))
